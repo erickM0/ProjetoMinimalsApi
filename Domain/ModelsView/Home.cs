6fa47f5f8ad7a885d38692912087f72766152386
erickM0 <145382051+erickM0@users.noreply.github.com>
2024-08-30 14:29:05 -0300

    feat: add ModelsView folder

---
+++ Domain/ModelsView/Home.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Minimal_Api.Domain.ModelsView
+{
+    public struct Home
+    {
+        public string Documentation{    get =>"/swagger";   }
+    }
+}
