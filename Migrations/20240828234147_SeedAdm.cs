d18f3dacaebfe6a52a946e94af3a2e0524791690
erickM0 <145382051+erickM0@users.noreply.github.com>
2024-08-30 14:28:16 -0300

    feat: Migrations

---
+++ Migrations/20240828234147_SeedAdm.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Minimal_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAdm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Administradores",
+                columns: new[] { "Id", "Email", "Password", "Profile" },
+                values: new object[] { 1, "teste@teste.com", "123456", "Admin" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Administradores",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
